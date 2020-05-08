33d6d30481c05ae106afbed984cff7fcbef2848a
Andrew Cameron <andrew@acameron.org>
2020-05-08 11:13:00 -0400

    adapt layout and setup script to use GNU Stow

--- setup.sh
+++ setup.sh
@@ -6,35 +6,12 @@
 # desired dotfiles (whose filenames begin with an '_', to be replaced by '.').
 ############################
 
-########## Variables
-
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files=`ls -d _*`                  # link all files in this repo starting with '_'
-files=`echo $files|sed s/_//g`    # derive the basenames of each file/directory
-
-##########
-
-# create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
-echo "...done"
-
-# change to the dotfiles directory
-echo "Changing to the $dir directory"
-cd $dir
-echo "...done"
-
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
-for file in $files; do
-    if [ -e ~/.$file ]; then
-        mv ~/.$file ~/dotfiles_old/
-    fi
-    ln -s $dir/_$file ~/.$file
-done
+# unstow all configurations by default
+# otherwise, unstow just the configurations provided as arguments
+stow -v ${@:-*/}
 
 # set up submodules
 git submodule update --init
 
 # run Vundle installer
-vim -u bundles.vim +BundleInstall +q
+vim -u vim/.vim/bundles.vim +BundleInstall +q
